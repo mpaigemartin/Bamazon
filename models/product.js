d70fcbcd344a933dc8d566fa3347c5661a491a0f
Paige <mpaigemartin@gmail.com>
2019-02-19 17:01:25 -0500

    css and sql

---
+++ models/product.js
@@ -0,0 +1,16 @@
+module.exports = function(app){
+const product = {
+    product_name: {
+        type = Sequelize.STRING
+    },
+    department_name: {
+        type = Sequelize.STRING
+    },
+    price: {
+        type = Sequelize.INTERGER
+    },
+    stock_quantity: {
+        type = Sequelize.INTERGER
+    },
+    }
+}
